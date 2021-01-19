f1ddcf33a97981b707a242148119804e8c1c3224
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2021-01-19 20:19:37 +0600

    Update pythonshowmyip.py

--- pythonshowmyip.py
+++ pythonshowmyip.py
@@ -4,4 +4,6 @@ os.system("clear")
 print ("      Python Show My IP  ")
 print ("Author : Rahat Khan Tusar(rkt)")
 print ("Github : https://github.com/r3k4t")
+print
 os.system("curl https://ipinfo.io/json")
+print
