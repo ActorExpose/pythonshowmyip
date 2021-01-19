43b7a39cfffbc68e91f6b9ca4715271f6c64cec6
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2021-01-19 20:09:33 +0600

    Create python-showmyip.py

---
+++ python-showmyip.py
@@ -0,0 +1,7 @@
+import os
+import sys
+os.system("clear")
+print ("      Python Show My IP  ")
+print ("Author : Rahat Khan Tusar(rkt)")
+print ("Github : https://github.com/r3k4t")
+os.system("curl https://ipinfo.io/json")
